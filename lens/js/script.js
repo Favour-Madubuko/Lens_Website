3147763269c574cf13ed8c9cdd68fb2eb0e5dd5b
Xperience0001 <favourmadubuko1@gmail.com>
2022-08-16 20:49:40 +0100

    Initial files

---
+++ lens/js/script.js
@@ -0,0 +1,4 @@
+const simpleLightbox = require('./node_modules/simplelightbox/dist/simple-lightbox.js');
+import SimpleLightbox from 'simple-lightbox';
+var gallery = $('.section-c .gallery a').simpleLightbox();
+gallery.next();
